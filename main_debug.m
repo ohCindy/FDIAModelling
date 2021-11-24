747ca5c666c2d030fe0194b1e56c3e92814dde58
Shimiao (Cindy) Li <shimiaol@andrew.cmu.edu>
2021-11-23 23:02:47 -0500

    added access limit to writing meters
    added access limit to writing meters
    test_sensitivity=1, tested_inaccuracy = 'meter access'

---
+++ main_debug.m
@@ -0,0 +1,24 @@
+N_trial = 200;
+Y_all = [];
+
+for i_trial = 1:N_trial
+    FDIA_modelling_main;
+    %save(sprintf('%s_y_%d.mat',casename(1:end-2),i_trial),'y')
+    Y_all = [Y_all;y];
+    %save(sprintf('%s_Yall_%d.mat',casename(1:end-2),i_trial),'Y_all')
+end
+
+% for i_trial = 1:N_trial
+%     load(sprintf('%s_y_%d.mat',casename(1:end-2),i_trial),'y')
+%     Y_all = [Y_all;y];
+% end
+
+%figure,plot(mean(Y_all)),hold on
+%plot(Y_all','bo')
+
+figure, boxplot(Y_all)
+
+xticks(1:length(y))
+xticklabels(x)
+
+
