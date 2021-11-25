d8dd73e5926e20f077eb26c6a85cf210e0cc9634
Shimiao (Cindy) Li <shimiaol@andrew.cmu.edu>
2021-11-24 21:46:59 -0500

    quantified the successfulness of attack by the percentage of wrong load estimate compared with no attack
    Suppose the attacker targets at misleading the operator into thinking the load increases a lot (by 20% the true value)
    Then total_Pload_diff = 0.2 means, the operator under attack makes a higher load estimate than the estimate without attack, by 20% of the true load.
    This means the attack is very successful.
    ----------------------------
    also if the attack aims to mislead the operator to estimate less load (80% the true value.)
    total_Pload_diff = -0.2: When the attack is launched, the estimate of Pload is less than the estimate without attack by 20% of the true value. The attack is very successful in misleading operators to wrong estimate.

--- main_debug.m
+++ main_debug.m
@@ -1,12 +1,18 @@
 N_trial = 200;
 Y_all = [];
+S_all = [];
 
 for i_trial = 1:N_trial
     FDIA_modelling_main;
     %save(sprintf('%s_y_%d.mat',casename(1:end-2),i_trial),'y')
-    Y_all = [Y_all;y];
+    Y_all = [Y_all;y_J]; 
+    S_all = [S_all;y_suc];
     %save(sprintf('%s_Yall_%d.mat',casename(1:end-2),i_trial),'Y_all')
+    %save(sprintf('%s_Sall_%d.mat',casename(1:end-2),i_trial),'S_all')
 end
+
+save(sprintf('%s_Yall_%d.mat',casename(1:end-2),i_trial),'Y_all')
+save(sprintf('%s_Sall_%d.mat',casename(1:end-2),i_trial),'S_all')
 
 % for i_trial = 1:N_trial
 %     load(sprintf('%s_y_%d.mat',casename(1:end-2),i_trial),'y')
@@ -17,8 +23,10 @@ end
 %plot(Y_all','bo')
 
 figure, boxplot(Y_all)
-
-xticks(1:length(y))
+xticks(1:length(x))
 xticklabels(x)
 
+figure, boxplot(S_all)
+xticks(1:length(x))
+xticklabels(x)
 
